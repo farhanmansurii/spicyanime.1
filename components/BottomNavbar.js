d057c41add60128e4a1a07d77a56f2fe52d9c5e5
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-10-07 20:15:40 +0530

    hi

---
+++ components/BottomNavbar.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+const BottomNavbar = () => {
+  const [active, setactive] = useState(false)
+  const router = useRouter();
+  return (
+    <div className="btm-nav fixed z-30 lg:hidden " >
+      <Link href='/' >
+        <button className={router.pathname == "/" ? "active" : ""}>
+          Home
+        </button>
+      </Link>
+      <Link href='/anime' >
+        <button className={router.pathname == "/anime" ? "active" : ""}>
+          Anime
+        </button>
+      </Link>
+      <Link href='/searchpage' >
+        <button className={router.pathname == "/searchpage" ? "active" : ""}>
+          search
+        </button>
+      </Link>
+
+    </div >
+  )
+}
+
+export default BottomNavbar;
