16242711151b00b9cad0b634e242f2847966e40e
farhan <mansurifarhanfm@gmail.com>
2022-10-08 02:40:05 +0530

    h

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["images.fanart.tv"],
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "",
-    nodeEnv: "development",
-  },
-};
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === 'development',
-    skipWaiting: true,
-  },
-});
-module.exports = nextConfig;
+})
