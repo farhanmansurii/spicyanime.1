8545a52478c719da27931c8538009d9a31aa440d
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-10-12 18:32:47 +0530

    s

---
+++ components/UseAuth.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+import { auth } from "../pages/firebase"
+const useAuth = () => {
+  const [user, setUser] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user)
+      setIsLoggedIn(user && user.uid ? true : false)
+    })
+  }, [user, isLoggedIn])
+  return { user, isLoggedIn }
+}
+export default useAuth
---
+++ pages/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGCUijj8MH3whjoPEfAAENQnM2vRrAgZc",
+  authDomain: "spicyanime-e1d48.firebaseapp.com",
+  projectId: "spicyanime-e1d48",
+  storageBucket: "spicyanime-e1d48.appspot.com",
+  messagingSenderId: "1099032984903",
+  appId: "1:1099032984903:web:52b1c15e46c8871f95e062",
+  measurementId: "G-C5RREFWD0E"
+};
+export default firebaseConfig
