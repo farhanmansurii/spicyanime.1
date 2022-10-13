26c0c8047206429306df29e3bf7ef1daa0032da9
Farhan Mansuri <mansurifarhanfm@gmail.com>
2022-10-13 13:34:04 +0530

    hi

--- components/Animedetails.js
+++ components/Animedetails.js
@@ -1,22 +1,19 @@
 import { addDoc, collection } from 'firebase/firestore';
 import React from 'react';
 import { db } from './config/firebase';
-const Animedetails = ({ deets, user }) => {
-  const addwatchlist = async ({ userId, id, title, }) => {
+const Animedetails = ({ deets, user, isLoggedIn }) => {
+  const addwatchlist = async () => {
     try {
-      await addDoc(collection(db, user.uid, 'watchlist'), {
-        user: userId,
-        title: title,
-        id: id
-      })
+      await addDoc(collection(db, 'watchlist'), data)
     } catch (err) {
       ''
     }
   }
   const data = {
-    userId: user.uid,
-    title: deets.title.english,
-    id: deets.id
+    userId: user?.uid,
+    title: deets.title.userPreferred || deets.title.romaji || deets.title.english,
+    id: deets.id,
+    image: deets.image
   }
 
   return (
@@ -42,7 +39,7 @@ const Animedetails = ({ deets, user }) => {
                 {deets.type}
               </div>
             </div>
-            <div > <div className="px-2 py-1 line-clamp-5 flex-row m-1 text-xs lg:text-lg bg-transparent backdrop-blur font-semibold text-primary/70 text-shadow-xl border-2 border-primary/20 rounded-sm w-fit">
+            <div > <button className="btn bg-secondary" onClick={addwatchlist}>Add to List</button><div className="px-2 py-1 line-clamp-5 flex-row m-1 text-xs lg:text-lg bg-transparent backdrop-blur font-semibold text-primary/70 text-shadow-xl border-2 border-primary/20 rounded-sm w-fit">
               Synopsis  {' '}
               : {'    '}
               <em>
@@ -58,10 +55,9 @@ const Animedetails = ({ deets, user }) => {
                   >
                     {e}
                   </div>
-                ))}<button className="btn btn-xs btn-circle btn-outline" onClick={() => addwatchlist(data)} >
+                ))}
 
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-                </button>
+
               </div>
             </div>
             <div className="px-2 py-1 flex m-1 text-xs lg:text-lg bg-transparent backdrop-blur font-semibold text-primary text-shadow-xl border-2 border-primary/20 rounded-sm w-fit">
--- pages/mylist.js
+++ pages/mylist.js
@@ -1,21 +1,47 @@
-import React from "react";
-import { auth } from "../components/config/firebase";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
+import AnimeCard from "../components/AnimeCard";
+import { auth, db } from "../components/config/firebase";
 const mylist = ({ isLoggedIn, handleAuth, user, watchlist, setwatchlist }) => {
+
+  const [animes, setAnimes] = React.useState([])
+  useEffect(() => {
+    const refreshData = () => {
+      if (!user) {
+        setAnimes([])
+        return
+      }
+      const q = query(collection(db, "watchlist"), where("userId", "==", user.uid))
+      onSnapshot(q, (querySnapchot) => {
+        let ar = []
+        querySnapchot.docs.forEach((doc) => {
+          ar.push({ id: doc.id, ...doc.data() })
+        })
+        setAnimes(ar)
+      })
+    }
+    refreshData()
+  }, [user])
+  console.log(animes)
   return (
     <div className="mx-10 text-white">
       {!isLoggedIn ?
 
         (
 
-          <button className="btn" onClick={() => handleAuth()}>sign In With Google</button>
+          <button className="btn" onClick={() => handleAuth()}>Google</button>
         ) : (
 
           <button className="btn" onClick={() => auth.signOut()}>Sign Out  </button>
         )
       }
-      <div className="text-primary"> {user?.displayName || ""}</div>
-      <div className="text-primary"> {watchlist}</div>
+      <div className="text-primary text-3xl"> {user?.displayName}'s WatchList</div>
+      <div className="p-5 grid my-10 grid-cols-2 gap-2 md:grid-cols-6  lg:w-10/12 mb-[6rem]">
 
+        {
+          animes.map((e) => <AnimeCard animeImg={e.image} title={e.title} id={e.id} key={e.id} />)
+        }
+      </div>
     </div>
   )
 }
