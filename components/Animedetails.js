3aaa302009a512a15d225ef205777532197eb558
farhan <mansurifarhanfm@gmail.com>
2022-10-17 01:34:21 +0530

    hi

--- components/Animedetails.js
+++ components/Animedetails.js
@@ -24,6 +24,7 @@ const Animedetails = ({ deets, user, animes }) => {
       isAdded: true,
     }
     if (user?.email) {
+      setIsAdded(true)
       await updateDoc(animeRef, {
         savedAnime: arrayUnion({
           userId: user?.uid,
