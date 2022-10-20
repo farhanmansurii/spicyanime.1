fd7a142f6c4cd1cc4cb16e3024cb9f968b7fffab
farhan <mansurifarhanfm@gmail.com>
2022-10-21 02:44:23 +0530

    h

--- pages/mylist.js
+++ pages/mylist.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import AnimeCard from "../components/AnimeCard";
 export default function Mylist({ user, isLoggedIn, handleAuth, watchlist, setwatchlist, signOut }) {
-  const [userid, setUserid] = useState()
-  console.log(watchlist)
   return (
-    <div className=" text-white">
-      {user ? (<div className="text-primary font-damion ml-5 text-3xl my-4"> {user?.displayName}&apos;s WatchList</div>) : (<div className="text-primary my-4 text-3xl font-damion "> Login To View Your Watchlist</div>)}
-      {watchlist ? (
+    <div >
+      {user ? (<div className="text-primary font-damion ml-5 w-10/12 text-3xl my-4"> {user?.displayName}&apos;s WatchList</div>) : (<div className="text-primary my-4  ml-4 text-3xl font-damion "> Login To View Your Watchlist</div>)}
+      {watchlist.length > 1 && (
         <div className="p-5 grid my-10 grid-cols-3 gap-2 md:grid-cols-6  lg:w-10/12 mb-[6rem]">
           {watchlist.map((e) => <AnimeCard animeImg={e.image} title={e.title} id={e.id} key={e.id} />)}
-        </div>) : (
-        <div className="text-primary text-2xl"> Login to View Your watchlist</div>
-      )
+        </div>)
       }
     </div>
   )
