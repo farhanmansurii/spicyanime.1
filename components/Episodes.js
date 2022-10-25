6d0a76be9f3a1b732f74537e19185b4e80cf322d
farhan <mansurifarhanfm@gmail.com>
2022-10-26 04:46:17 +0530

    hi

--- components/Episodes.js
+++ components/Episodes.js
@@ -4,19 +4,24 @@ import ReactPlayer from 'react-player';
 import EpisodeCard from './EpisodeCard';
 const Episodes = ({ epi, deets, user }) => {
   console.log(deets)
-  const [eplink, seteplink] = React.useState()
+
+  const [eplink, seteplink] = React.useState(epqual?.url)
+  const [epqual, setepqual] = React.useState()
+  const [epqualities, setepqualities] = React.useState()
   const [epid, setepid] = React.useState(deets.episodes[0].id || deets.episode[1].id)
   const [episodedeets, setepisodedeets] = useState({ number: deets.episodes[0].number, title: deets.episodes[0].title, description: epi[0].description } || { number: deets.episodes[1].number, title: deets.episodes[1].title, description: epi[1].description })
   function epfetch() {
     fetch(
-      "https://animeapi-demo.herokuapp.com/animix/watch/" + epid)
+      "https://api.consumet.org/anime/gogoanime/watch/" + epid)
       .then((res) => res.json())
       .then((json) => {
-        seteplink(json.sources[0].file ? json.sources[0].file : json.sources[1].file)
-        console.log(json)
+        setepqual(json.sources)
+        setepqualities(json.sources?.find(e => e.quality === 'default'))
+        console.log(json.sources?.find(e => e.quality === 'default'))
+        seteplink(epqualities?.url)
       });
   }
-
+  console.log()
   const [initial, setinitial] = useState(0)
   const [final, setfinal] = useState(24)
   const [curr, setcurr] = useState(1)
@@ -29,17 +34,18 @@ const Episodes = ({ epi, deets, user }) => {
   }
   useEffect(() => {
     epfetch()
+
     return () => {
     }
   }, [epid])
   return (<>
-    <div className=" place-self-center my-5 border-0 w-fit bg-black/30 mx-auto whitespace-wrap ">
+    {eplink && <div className=" place-self-center my-5 border-0 w-fit bg-black/30 mx-auto whitespace-wrap ">
       <div className='flex flex-auto justify-between mr-5 m-3'>
         <div>
           {deets.type !== "MOVIE" ? (<div className=" mx-auto pt-5 px-5 text-md  text-primary font-damion normal-case line-clamp-2"  > EP {episodedeets.number} :{episodedeets.title} </div>
           ) : <div className=" mx-auto pt-5 px-5 text-md  text-primary font-damion normal-case line-clamp-2" > Movie</div>}
         </div>
-        {deets.type !== "MOVIE" && <div className='w-fit btn  text-primary  normal-case font-damion bg-base-100/50 border-0 text-md my-auto' onClick={() => nextep()}>Ep {curr + 1}  <MdOutlineNavigateNext /></div>
+        {deets.type !== "MOVIE" && <div className='w-fit btn  btn-sm font-normal  text-primary  normal-case font-damion bg-base-100/50 border-0 text-md my-auto' onClick={() => nextep()}>Next episode  <MdOutlineNavigateNext /></div>
         }</div>
 
       <ReactPlayer
@@ -47,21 +53,30 @@ const Episodes = ({ epi, deets, user }) => {
         height='360'
         width='640'
         url={eplink} />
-    </div>
+    </div>}
+    {epqual &&
+      <>
+        <div className='text-sm font-damion text-primary'> Select Quality : </div>
+        {epqual.map((e, index) =>
+          <button className='btn btn-xs bg-secondary text-primary font-normal normal-case font-damion ' onClick={() => seteplink(e.url)} key={index}>
+            {e.quality}
+          </button>)}
+      </>
+    }
     <div className='flex flex-row w-full  my-4'>
 
 
       {deets.totalEpisodes > 25 && (<div className="btn-group hover:bg-transparent btn-ghost align-end  w-10/12   ">
-        {initial !== 0 && <button className="btn btn-primary border-0  bg-base-100    text-primary " onClick={() => { setinitial(initial - 24), setfinal(final - 24) }}><MdOutlineArrowBack className='w-8 h-8' /></button>}
+        {initial !== 0 && <button className="btn btn-primary border-0  bg-base-100  hover:bg-secondary  text-primary " onClick={() => { setinitial(initial - 24), setfinal(final - 24) }}><MdOutlineArrowBack className='w-8 h-8' /></button>}
 
-        {final < deets.totalEpisodes && (<button className=" btn btn-primary border-0  bg-base-100    text-primary " onClick={() => { setinitial(initial + 24), setfinal(final + 24) }} ><MdOutlineArrowForward className='w-8 h-8' /></button>)}
+        {final < deets.totalEpisodes && (<button className=" btn btn-primary border-0 hover:bg-secondary  bg-base-100    text-primary " onClick={() => { setinitial(initial + 24), setfinal(final + 24) }} ><MdOutlineArrowForward className='w-8 h-8' /></button>)}
       </div>)}
       <div className=" my-auto  mx-2 text-2xl font-damion  text-primary whitespace-nowrap ">
         Episode {initial + 1} - {final < epi.length ? final + 1 : epi.length}
       </div>
     </div>
     <div className=" flex overflow-x-scroll  scrollbar-hide ">
-      {epi.slice(initial, final).map((e) => (<div key={e.id} className="flex flex-col-reverse bg-cover ease-in transition duration-100 transform sm:hover:scale-105 rounded-[10px]  h-[113px] lg:h-[200px] w-[200px] lg:w-[300px] m-2 " onClick={() => { setepid(e.id), setepisodedeets({ number: e.number, title: e.title, description: e.description }) }}>
+      {epi.slice(initial, final).map((e) => (<div key={e.id} className="flex flex-col-reverse bg-cover ease-in transition duration-100 transform sm:hover:scale-105 rounded-[10px]  h-[113px] lg:h-[200px] w-[200px] lg:w-[300px] m-2 " onClick={() => { setepid(e.id), setcurr(e.number), setepisodedeets({ number: e.number, title: e.title, description: e.description }) }}>
         <EpisodeCard episode={e} id={e.id} user={user} />
       </div>
       ))}
