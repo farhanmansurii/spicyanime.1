79303c6a37af97c67ec8c8d879d2cc6e283c87a4
Farhan <mansurifarhanfm@gmail.com>
2022-12-31 04:53:09 +0530

    h

--- components/Banner.js
+++ components/Banner.js
@@ -20,8 +20,11 @@ export const Banner = ({ titles, description, imageban, id }) => {
             )}
           </div>
           <div className="flex">
-            <Link href={`/details?id=${id}`}><div className="  font-damion  hover:scale-95 duration-150 hover:bg-secondary-focus  px-6 py-3 rounded-full bg-secondary uppercase text-primary ">
-              Play
+            <Link href={`/details?id=${id}`}><div className=" flex  font-damion  hover:scale-95 duration-150 hover:bg-secondary-focus  px-6 py-3 rounded-full bg-secondary gap-2 text-primary ">
+              Play <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 my-auto">
+                <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
+              </svg>
+
             </div></Link>
 
           </div>
