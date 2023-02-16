57d037f0582188bfba5ee05b9745180e631b7da4
Farhan <mansurifarhanfm@gmail.com>
2023-02-16 14:30:48 +0530

    new test

--- pages/details.js
+++ pages/details.js
@@ -1,17 +1,14 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import axios from "axios";
-import dynamic from "next/dynamic";
 import React, { useEffect, useState } from "react";
 import Spinner from "react-spinner-material";
 import Animedetails from "../components/Animedetails";
 import Row from "../components/Row";
-const Episodes = dynamic(() => import("../components/Episodes"), {
-  loading: () => <div className='w-fit h-full ease-in-out duration-200 grid justify-center mx-auto place-content-center'>
-
-    <Spinner radius={30} color='#DA0037' stroke={5} visible={true} />
-  </div>
-  , ssr: false
-});
+// const Episodes = dynamic(() => import("../components/Episodes"), {
+//   loading: () => 
+//   , ssr: false
+// });
+import Episodes from "../components/Episodes";
 function details({ deets, setwatchlist, watchlist, user, related, animen, notFound }) {
   const [epi, setEpi] = useState([]);
   const [epIsLoading, setepIsLoading] = useState(<Spinner radius={30} color='#DA0037' stroke={5} visible={true} />);
@@ -38,13 +35,15 @@ function details({ deets, setwatchlist, watchlist, user, related, animen, notFou
           <Animedetails deets={deets} animen={animen} epi={epi} watchlist={watchlist} setwatchlist={setwatchlist} user={user} />
         </div>
       )}
-      {epi.length > 0 && (
+      {epi.length > 0 ? (
         <div className="w-full mx-auto">
           <Episodes deets={deets} user={user} watchlist={watchlist} setwatchlist={setwatchlist} epi={epi} />
         </div>
-      )}
-      {!epi && (
-        <div className="mx-auto text-2xl font-damion place-text-center my-6 text-center text-primary">Loading episodes</div>
+      ) : (
+        <div className='w-fit h-full ease-in-out duration-200 grid justify-center mx-auto place-content-center'>
+
+          <Spinner radius={30} color='#DA0037' stroke={5} visible={true} />
+        </div>
       )}
       <div className="mb-24 lg:pb-10">
 
@@ -56,34 +55,21 @@ function details({ deets, setwatchlist, watchlist, user, related, animen, notFou
 }
 export async function getServerSideProps(context) {
   const animeId = context.query.id;
-
-  try {
-    const detailsResponse = await fetch(`https://api.amvstr.ml/api/v2/info/${animeId}`);
-    const details = await detailsResponse.json();
+  const detailsResponse = await fetch(`https://api.amvstr.ml/api/v2/info/${animeId}`);
+  const details = await detailsResponse.json();
 
 
-    const relatedResponse = await fetch(`https://api.amvstr.ml/api/v2/recommendations/${animeId}`);
-    const related = await relatedResponse.json();
+  const relatedResponse = await fetch(`https://api.amvstr.ml/api/v2/recommendations/${animeId}`);
+  const related = await relatedResponse.json();
 
-    return {
-      props: {
-        deets: details.data,
-        animen: animeId,
-        notFound: false,
-        related: related.data
-      }
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        deets: details?.data,
-        animen: animeId,
-        related: related?.data,
-        notFound: true
-      }
-    };
-  }
+  return {
+    props: {
+      deets: details.data,
+      animen: animeId,
+      notFound: false,
+      related: related.data
+    }
+  };
 }
 
 
