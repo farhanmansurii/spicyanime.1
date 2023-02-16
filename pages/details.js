ab755f2502505b7b7d51d7d7a64bc17247584d19
Farhan <mansurifarhanfm@gmail.com>
2023-02-16 14:16:14 +0530

    new again

--- pages/details.js
+++ pages/details.js
@@ -38,17 +38,10 @@ function details({ deets, setwatchlist, watchlist, user, related, animen, notFou
           <Animedetails deets={deets} animen={animen} epi={epi} watchlist={watchlist} setwatchlist={setwatchlist} user={user} />
         </div>
       )}
-      {epi.length > 0 ? (
+      {epi && (
         <div className="w-full mx-auto">
           <Episodes deets={deets} user={user} watchlist={watchlist} setwatchlist={setwatchlist} epi={epi} />
         </div>
-      ) : epi.length === 0 ? (
-        <div className="mx-auto text-2xl font-damion place-text-center my-6 text-center text-primary">No Data Found <button onClick={() => fetchData()}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-        </svg>
-        </button></div>
-      ) : (
-        <div className="mx-auto text-2xl font-damion place-text-center my-6 text-center text-primary">Loading episodes</div>
       )}
       <div className="mb-24 lg:pb-10">
 
